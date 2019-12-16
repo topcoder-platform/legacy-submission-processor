b4cac1fc7fbfbd043ccc3ae1b13f7aa5fbc47b36
Dushyant Bhalgami <dushyantbhalgami@gmail.com>
2019-12-16 17:21:31 +0530

    added circleci configs

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eo pipefail
+UPDATE_CACHE=""
+docker-compose -f docker/docker-compose.yml build legacy-submission-processor
+docker create --name app legacy-submission-processor:latest
+
+if [ -d node_modules ]
+then
+  mv package-lock.json old-package-lock.json
+  docker cp app:/legacy-submission-processor/package-lock.json package-lock.json
+  set +eo pipefail
+  UPDATE_CACHE=$(cmp package-lock.json old-package-lock.json)
+  set -eo pipefail
+else
+  UPDATE_CACHE=1
+fi
+
+if [ "$UPDATE_CACHE" == 1 ]
+then
+  docker cp app:/legacy-submission-processor/node_modules .
+fi
